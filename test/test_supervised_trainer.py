2574eb69fb70bfeebc8f11b46a0abfcb2134f02b
Dieuwke <dieuwkehupkes@gmail.com>
2017-12-20 09:27:50 +0100

    Fix supervised trainer tests
    Add mock evaluator to be able to run train_epoches function from
    supervised_trainer. Tests run now without problem again (refer to commit
    dbc0ed178a).

--- test/test_supervised_trainer.py
+++ test/test_supervised_trainer.py
@@ -20,34 +20,35 @@ class TestSupervisedTrainer(unittest.TestCase):
         src.build_vocab(self.dataset)
         tgt.build_vocab(self.dataset)
 
-#     @mock.patch('seq2seq.trainer.SupervisedTrainer._train_batch', return_value=0)
-#     @mock.patch('seq2seq.util.checkpoint.Checkpoint.save')
-#     def test_batch_num_when_resuming(self, mock_checkpoint, mock_func):
-#         mock_model = mock.Mock()
-#         mock_optim = mock.Mock()
-# 
-#         trainer = SupervisedTrainer(batch_size=16)
-#         trainer.optimizer = mock_optim
-#         n_epoches = 1
-#         start_epoch = 1
-#         steps_per_epoch = 7
-#         step = 3
-#         trainer._train_epoches(self.dataset, mock_model, n_epoches, start_epoch, step)
-# 
-#         self.assertEqual(steps_per_epoch - step, mock_func.call_count)
+    @mock.patch('seq2seq.trainer.SupervisedTrainer._train_batch', return_value=0)
+    @mock.patch('seq2seq.util.checkpoint.Checkpoint.save')
+    @mock.patch('seq2seq.evaluator.Evaluator.evaluate', return_value=(0,0))
+    def test_batch_num_when_resuming(self, mock_evaluator, mock_checkpoint, mock_func):
+        mock_model = mock.Mock()
+        mock_optim = mock.Mock()
 
-#     @mock.patch('seq2seq.trainer.SupervisedTrainer._train_batch', return_value=0)
-#     @mock.patch('seq2seq.util.checkpoint.Checkpoint.save')
-#     def test_resume_from_multiple_of_epoches(self, mock_checkpoint, mock_func):
-#         mock_model = mock.Mock()
-#         mock_optim = mock.Mock()
-# 
-#         trainer = SupervisedTrainer(batch_size=16)
-#         trainer.optimizer = mock_optim
-#         n_epoches = 1
-#         start_epoch = 1
-#         step = 7
-#         trainer._train_epoches(self.dataset, mock_model, n_epoches, start_epoch, step, dev_data=self.dataset)
+        trainer = SupervisedTrainer(batch_size=16)
+        trainer.optimizer = mock_optim
+        n_epoches = 1
+        start_epoch = 1
+        steps_per_epoch = 7
+        step = 3
+        trainer._train_epoches(self.dataset, mock_model, n_epoches, start_epoch, step)
+        self.assertEqual(steps_per_epoch - step, mock_func.call_count)
+
+    @mock.patch('seq2seq.trainer.SupervisedTrainer._train_batch', return_value=0)
+    @mock.patch('seq2seq.util.checkpoint.Checkpoint.save')
+    @mock.patch('seq2seq.evaluator.Evaluator.evaluate', return_value=(0,0))
+    def test_resume_from_multiple_of_epoches(self, mock_evaluator, mock_checkpoint, mock_func):
+        mock_model = mock.Mock()
+        mock_optim = mock.Mock()
+
+        trainer = SupervisedTrainer(batch_size=16)
+        trainer.optimizer = mock_optim
+        n_epoches = 1
+        start_epoch = 1
+        step = 7
+        trainer._train_epoches(self.dataset, mock_model, n_epoches, start_epoch, step, dev_data=self.dataset)
 
 if __name__ == '__main__':
     unittest.main()
