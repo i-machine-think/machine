c724c2def9030769cc585630908dafe8d183e5bb
Dieuwke <dieuwkehupkes@gmail.com>
2017-12-21 10:03:36 +0100

    update imports pcredict.py

--- predict.py
+++ predict.py
@@ -3,16 +3,9 @@ import argparse
 import logging
 
 import torch
-from torch.optim.lr_scheduler import StepLR
-import torchtext
 
 import seq2seq
-from seq2seq.trainer import SupervisedTrainer
-from seq2seq.models import EncoderRNN, DecoderRNN, Seq2seq
-from seq2seq.loss import Perplexity
-from seq2seq.optim import Optimizer
-from seq2seq.dataset import SourceField, TargetField
-from seq2seq.evaluator import Predictor, Evaluator
+from seq2seq.evaluator import Predictor
 from seq2seq.util.checkpoint import Checkpoint
 
 try:
