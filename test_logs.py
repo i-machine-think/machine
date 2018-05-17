9bdf2702db621273e84c96f721804e028b26aaee
Dieuwke <dieuwkehupkes@gmail.com>
2018-05-17 18:15:58 +0200

    Some new functions and scripts

--- test_logs.py
+++ test_logs.py
@@ -1,13 +1,21 @@
 from seq2seq.util.log import LogCollection
+import re
 
 def name_parser(filename, subdir):
     splits = filename.split('/')
     return splits[1]+'_'+splits[-2]
 
 log = LogCollection()
-log.add_log_from_folder('dumps', ext='.dump', name_parser=name_parser)
+log.add_log_from_folder('chosens_dump', ext='.dump', name_parser=name_parser)
 
-print(log.log_names)
+############################
+# helper funcs
+
+def natural_sort(l): 
+    convert = lambda text: int(text) if text.isdigit() else text.lower() 
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
+    return sorted(l, key = alphanum_key)
+
 
 def func(input_str):
     if 'full_focus' in input_str and 'hard' not in input_str and 'baseline' not in input_str:
@@ -19,30 +27,152 @@ def f64_256(input_str):
         return True
     return False
 
-def data_name_parser(input_str):
-    return ''
-
-
-def func2(input_str):
-    if 'longer_compositions' in input_str:
+def pre_rnn(input_str):
+    if 'pre_rnn' in input_str\
+            and 'baseline' not in input_str and 'hard' not in input_str:
         return True
     return False
 
-def color_group(input_str):
-    if 'baseline' in input_str:
+def full_focus(input_str):
+    if 'full_focus' in input_str\
+            and 'baseline' not in input_str and 'hard' not in input_str:
+            # and 'E64xH512' in input_str:
+        return True
+    return False
+
+def ff_and_baseline(input_str):
+    if ('full_focus' in input_str or 'baseline' in input_str):
+        return True
+    return False
+
+def pre_and_baseline(input_str):
+    if 'pre_rnn' in input_str and 'hard' not in input_str\
+            and 'H16' not in input_str and 'H32' not in input_str:
+        return True
+    return False
+
+def best_pre_and_baseline(input_str):
+    if 'pre_rnn' in input_str and ( \
+            ('hard' not in input_str and 'E16xH512' in input_str \
+            and 'baseline' not in input_str) \
+            or ('baseline' in input_str and 'E128xH512' in input_str)):
+        return True
+    return False
+
+def hard(input_str):
+    if 'hard' in input_str and 'pre_rnn' in input_str:
+        return True
+    return False
+
+def baseline(model):
+    if 'baseline' in model and 'full_focus' in model:
+            # and ('E16xH256' in model or 'E62xH256'  in model or 'E64xH512' in model):
+        return True
+    return False
+
+def data_name_parser(data_name, model_name):
+    return input_str.split('/')[-1].split('.')[0]
+
+def data_name_parser(data_name, model_name):
+    if 'Train' in data_name and 'baseline' in model_name:
+        label = 'Baseline, training loss'
+    elif 'Train' in model_name:
+        label = 'Attention Guidance, Train'
+    elif 'baseline' in model_name:
+        label = 'Baseline, test loss'
+    else:
+        label = 'Attention Guidance, test loss'
+    return label
+
+def heldout_tables(input_str):
+    if 'heldout_tables' in input_str:
+        return True
+    return False
+
+def not_longer(input_str):
+    if 'longer' not in input_str:
+        return True
+    return False
+
+def not_train(dataset):
+    if 'Train' not in dataset:
+        return True
+    return False
+
+def color_train(model_name, data_name):
+    if 'Train' in data_name and 'baseline' in model_name:
+        c = 'k--'
+    elif 'Train' in data_name:
+        c = 'k'
+    elif 'baseline' in model_name:
+        c = 'm:'
+    else:
+        c = 'g'
+
+    return c
+
+def color_group(model_name, data_name):
+    if 'baseline' in model_name:
         c = 'b'
-    elif 'hard' in input_str:
+    elif 'hard' in model_name:
         c = 'm'
     else:
         c = 'g'
 
-    if 'pre_rnn' in input_str:
+    if 'pre_rnn' in model_name:
         l = ':'
-    elif 'full_focus' in input_str:
+    elif 'full_focus' in model_name:
         l = '-'
-    elif 'post_rnn' in input_str:
+    elif 'post_rnn' in model_name:
         l = '--'
 
     return c+l
 
-log.plot_metric('acc', restrict_model=f64_256, restrict_data=func2, data_name_parser=data_name_parser, color_group=color_group)
+def find_basename(model_name):
+    all_parts = model_name.split('_')
+    basename = '_'.join(all_parts[2:])
+    return basename
+
+def no_basename(model_name):
+    return model_name
+
+def find_data_name(dataset):
+    dataname = dataset.split('/')[-1].split('.')[0]
+    if 'longer' in dataname:
+        splits = dataname.split('_')
+        elements = [splits[0],splits[2]]
+        dataname = '_'.join(elements)
+    return dataname
+
+def color_baseline(model_name, data_name):
+    print model_name
+    if 'baseline' in model_name:
+        c = 'k'
+    else:
+        c = 'g'
+    return c
+
+max_averages = log.find_highest_average('seq_acc', find_basename=no_basename, find_data_name=find_data_name, restrict_data=not_longer)
+
+for model in natural_sort(max_averages):
+    datadict = max_averages[model]
+    print('%s:\t%s' % (model, '\t'.join(['%s %.2f' % (d, datadict[d]) for d in datadict])))
+
+exit()
+
+# log.plot_metric('seq_acc', restrict_model=full_focus, restrict_data=not_longer)
+
+def plot_pre_and_baseline():
+    # plot accuracy of all validation sets for best configuration for learned
+    # attention (pre) and baseline models to show overfitting
+    fig = log.plot_metric('nll_loss', restrict_model=best_pre_and_baseline, restrict_data=not_longer, data_name_parser=data_name_parser, color_group=color_train, eor=300)
+    fig.savefig('/home/dieuwke/Documents/papers/AttentionGuidance/figures/best_config_all_sets_loss.png')
+
+def plot_heldout_tables_all():
+    # all_models_heldout_tables
+    fig = log.plot_metric('seq_acc', restrict_model=pre_and_baseline, restrict_data=heldout_tables, color_group=color_baseline, eor=300)
+    fig.savefig('/home/dieuwke/Documents/papers/AttentionGuidance/figures/all_models_heldout_tables.png')
+
+
+# plot_pre_and_baseline()
+plot_heldout_tables_all()
---
+++ write_results.py
@@ -0,0 +1,92 @@
+from seq2seq.util.log import LogCollection
+from get_iterations import find_best_its
+
+import numpy as np
+
+from collections import defaultdict
+
+def name_parser(filename, subdir):
+    splits = filename.split('/')
+    return splits[1]+'_'+splits[2]+'_'+splits[-2]
+
+def data_name_parser(filename):
+    splits = filename.split('/')
+    data_name = splits[-1][:-4]
+    return data_name
+
+def baseline_prernn(modelname):
+    if 'baseline' in modelname and 'pre' in modelname:
+        return True
+    return False
+
+def prernn(modelname):
+    if 'pre' in modelname and 'baseline' not in modelname \
+            and 'hard' not in modelname:
+        return True
+    return False
+
+def group_runs_and_samples(model):
+    basename = ' '.join(model.split('_')[2:])
+    return basename
+
+def get_all_accuracies(log, acc_name, datasets, data_name_parser, best_its):
+
+    accuracies = {}
+
+    for i, log_name in enumerate(log.log_names):
+        accuracies[log_name] = {}
+        cur_log = log.logs[i]
+
+        # decide iteration to pick
+        index = cur_log.steps.index(best_its[log_name])
+
+        for dataset in cur_log.data.keys():
+            short_data_name = data_name_parser(dataset)
+            if short_data_name in datasets:
+                accuracies[log_name][short_data_name] = \
+                        cur_log.data[dataset][acc_name][index]
+
+    return accuracies
+
+def average_accuracies(accuracies, group_models):
+    av_accs = defaultdict(lambda: defaultdict(list))
+
+    for model in accuracies:
+        model_base = group_models(model)
+        for dataset, val in accuracies[model].items():
+            av_accs[model_base][dataset].append(val)
+
+    for model, model_accs in av_accs.items():
+        for dataset in model_accs:
+            d = av_accs[model][dataset]
+            av_accs[model][dataset] = (np.mean(d), np.std(d), np.min(d), np.max(d))
+    return av_accs
+
+def write_to_csv(accs, filename, model_parser, model_write):
+    f = open(filename, 'wb')
+    f.write('model, compName, seqacc, std, min, max\n')
+    for model in accs:
+        if model_parser(model):
+            for dataset, acc in av_accs[model].items():
+                f.write('%s, %s, %f, %f, %f, %f\n' % (model_write, dataset.replace('_', ' '), acc[0], acc[1], acc[2], acc[3]))
+    f.close()
+
+# create log
+log = LogCollection()
+log.add_log_from_folder('chosens_dump', ext='.dump', name_parser=name_parser)
+
+# find best iterations for ever file in log (given file with model names
+best_its = find_best_its('chosens_dump/tree.txt')
+
+datasets = ['heldout_inputs', 'heldout_compositions', \
+            'heldout_tables', 'new_compositions']
+
+accs = get_all_accuracies(log, 'seq_acc', datasets, data_name_parser, best_its)
+av_accs = average_accuracies(accs, group_runs_and_samples)
+write_to_csv(av_accs,  'pre_rnn_results_learned.csv', prernn, 'Guided')
+# write_to_csv(av_accs,  'pre_rnn_results_baseline.csv', baseline_prernn, 'Baseline')
+
+#for model in av_accs:
+#    m_accs = av_accs[model]
+#    m_short = '_'.join(model.split('_')[:-1])
+#    print('%s:\t\t%s' % (m_short, '\t'.join(['%s %.2f' % (m, m_accs[m]) for m in m_accs])))
