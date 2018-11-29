54c6841577f7932ea120bdc341e1e92764b9e8e4
Gautier Dagan <gautierdagan2017@u.northwestern.edu>
2018-11-29 11:41:46 +0100

    Fix misnamed var

--- LM_baseline.py
+++ LM_baseline.py
@@ -140,7 +140,7 @@ def train():
             elapsed = time.time() - start_time
             print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | ms/batch {:5.2f} | '
                   'loss {:5.2f} | ppl {:8.2f}'.format(
-                      epoch, i, len(train_data) // 35, lr,
+                      epoch, i, len(train_iter) // 35, lr,
                       elapsed * 1000 / log_interval, cur_loss, math.exp(cur_loss)))
             total_loss = 0
             start_time = time.time()
