456ec8744aac451cdeb0a5126ac5701fdc916d77
Gautier Dagan <gautierdagan2017@u.northwestern.edu>
2018-12-12 16:46:05 +0100

    BUG: Add pyyaml to reqs, fix torch warning in loss test

--- test/test_loss_loss.py
+++ test/test_loss_loss.py
@@ -8,6 +8,7 @@ import torch.nn.functional as F
 from machine.loss.loss import Loss
 from machine.loss import NLLLoss, Perplexity
 
+
 class TestLoss(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
@@ -16,25 +17,27 @@ class TestLoss(unittest.TestCase):
         num_batch = 10
         cls.num_batch = num_batch
         cls.outputs = [F.softmax(torch.randn(batch_size, num_class), dim=1)
-                   for _ in range(num_batch)]
+                       for _ in range(num_batch)]
         cls.targets = [torch.LongTensor([random.randint(0, num_class - 1)
-                                              for _ in range(batch_size)])
-                   for _ in range(num_batch)]
+                                         for _ in range(batch_size)])
+                       for _ in range(num_batch)]
 
     def test_loss_init(self):
         name = "name"
         shortname = "shortname"
-        inputs="decoder_output"
-        targets="decoder_output"
+        inputs = "decoder_output"
+        targets = "decoder_output"
         loss = Loss(name, shortname, inputs, targets, torch.nn.NLLLoss())
         self.assertEqual(loss.name, name)
         self.assertEqual(loss.log_name, shortname)
 
     def test_loss_init_WITH_NON_LOSS(self):
-        self.assertRaises(ValueError, lambda: Loss("name", "shortname", "decoder_outputs", "decoder_targets", "loss"))
+        self.assertRaises(ValueError, lambda: Loss(
+            "name", "shortname", "decoder_outputs", "decoder_targets", "loss"))
 
     def test_loss_backward_WITH_NO_LOSS(self):
-        loss = Loss("name", "shortname", "decoder_output", "decoder_output", torch.nn.NLLLoss())
+        loss = Loss("name", "shortname", "decoder_output",
+                    "decoder_output", torch.nn.NLLLoss())
         self.assertRaises(ValueError, lambda: loss.backward())
 
     def test_nllloss_init(self):
@@ -59,7 +62,7 @@ class TestLoss(unittest.TestCase):
     def test_nllloss_WITH_OUT_SIZE_AVERAGE(self):
         loss = NLLLoss(size_average=False)
         pytorch_loss = 0
-        pytorch_criterion = torch.nn.NLLLoss(size_average=False)
+        pytorch_criterion = torch.nn.NLLLoss(reduction='sum')
         for output, target in zip(self.outputs, self.targets):
             loss.eval_step(output, target)
             pytorch_loss += pytorch_criterion(output, target)
